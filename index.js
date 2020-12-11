103128cd839e307e6249c4b82e09a30841a14f4a
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-12-11 13:15:35 +0600

    final commit

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRouters from './routes/posts.js';
+import dotenv from "dotenv"
+
 
 const app = express();
-
+dotenv.config()
 app.use(bodyParser.json({ limit: "1mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "1mb", extended: true }));
 app.use(cors());
 app.use('/cars', postRouters)
 
 
-const CONNECTION_URL = 'mongodb+srv://iftekhar:94toOqv02C1v9E8z@cluster0.bqcea.mongodb.net/BSCarList?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqcea.mongodb.net/BSCarList?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
